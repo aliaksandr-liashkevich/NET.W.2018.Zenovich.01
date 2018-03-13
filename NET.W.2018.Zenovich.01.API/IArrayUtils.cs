f1fc5a8626a41c34ddfec3e1a30ccde74d0c931d
azenovich <alexander.zenovich@gmail.com>
2018-03-13 11:58:28 +0300

    Init project

---
+++ NET.W.2018.Zenovich.01.API/IArrayUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.W._2018.Zenovich._01.API
+{
+    public interface IArrayUtils
+    {
+        void QuickSort<T>(T[] array)
+            where T: IComparable;
+        void MergerSort<T>(T[] array)
+            where T: IComparable;
+    }
+}
---
+++ NET.W.2018.Zenovich.01.Model/ArrayUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NET.W._2018.Zenovich._01.API;
+
+namespace NET.W._2018.Zenovich._01.Model
+{
+    public class ArrayUtils : IArrayUtils
+    {
+
+        public void MergerSort<T>(T[] array) 
+            where T : IComparable
+        {
+            throw new NotImplementedException();
+        }
+
+        public void QuickSort<T>(T[] array) 
+            where T : IComparable
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
